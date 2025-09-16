e5ab46ce1a0a5b5d5901ae10c9ccaef892431382
Menez091 <Samiramenezes091@gmail.com>
2025-09-16 10:55:42 -0300

    Create try_except.py

---
+++ Modulo 1/Exercicios/try_except.py
@@ -0,0 +1,8 @@
+ numero=int(input("digite o numero:"))
+    onde_começa=int(input("digite o numero que vc quer começar:"))
+    onde_acaba=int(input("digite em que numero vc quer que acabe:"))
+    for i in range(onde_começa,onde_acaba):
+    for i in range(onde_começa,onde_acaba +1):
+        print(f"{i} x {numero}={i * numero}")
+except:
+    print("digite um numero correto!")
