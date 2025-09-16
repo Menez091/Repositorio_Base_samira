e2630ae33ca3bb182f84c6f5da1e266f22f4b3e3
Menez091 <Samiramenezes091@gmail.com>
2025-09-16 09:54:32 -0300

    Create imc.py

---
+++ Modulo 1/Exercicios/imc.py
@@ -0,0 +1,12 @@
+nome=str(input("digite seu nome?"))
+peso=float(input("qual é o seu peso?"))
+altura=float(input("qual a sua altura?"))
+
+IMC=(peso/altura * altura)
+
+if peso>=30:
+    print("pescisa se cuidar mais")
+elif  peso>=60:
+    print("tudo ok")
+elif peso>=80:
+    print("precisa fazer exercicios e ter uma alimentação regulada")
